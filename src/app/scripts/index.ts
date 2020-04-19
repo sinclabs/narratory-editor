4d5f4d21b828dc519dcbae9f1786cd1a3396d171
Subbu <subbu@furhatrobotics.com>
2020-04-19 15:18:01 +0200

    Hack to get rid of import issues

--- src/app/scripts/index.ts
+++ src/app/scripts/index.ts
@@ -1,6 +1,3 @@
-// @ts-check
-import { Agent } from "../../interfaces";
-
 // Script run within the webview itself.
 (function () {
 
@@ -42,9 +39,11 @@ import { Agent } from "../../interfaces";
 
 		// Render the scratches
 		editorContainer.innerHTML = '';
+		
 		const agentName = document.createElement('p');
 		agentName.innerText = `Agent Name: ${json.agentName}`;
 		editorContainer.append(agentName);
+		
 		// for (const note of json.scratches || []) {
 		// 	const element = document.createElement('div');
 		// 	element.className = 'note';
@@ -98,3 +97,352 @@ import { Agent } from "../../interfaces";
 		updateContent(state.text);
 	}
 }());
+
+// Interfaces ==> Index.ts
+interface Agent {
+	agentName: string
+	language: Language
+	narrative: Array<AbstractBotTurn | string | string[]>
+	userInitiatives?: Array<UserTurn>
+	botInitiatives?: Array<AbstractBotTurn>
+	bridges?: string[] | BotTurn[]
+	defaultFallbacks?: string[]
+	narratoryKey: string
+	googleCredentials: GoogleCredentials
+	maxMessagesPerTurn?: 1 | 2
+	allowGateway?: boolean
+	skipQueryRepeat?: boolean
+	logWebhook?: string
+	logLevel?: "NONE" | "FALLBACKS" | "ALL"
+}
+
+interface GoogleCredentials {
+	project_id: string
+	private_key: string
+	client_email: string
+	[key: string]: any
+}
+
+interface Enum {
+	name: string
+	alts?: string[]
+}
+
+interface AbstractEntity {
+	name: string
+	default?: string
+}
+
+interface Entity extends AbstractEntity {
+	enums: Enum[]
+}
+
+interface DynamicEntity extends Entity {
+	url: string
+	type: "BUILD" | "SESSION" | "TURN"
+}
+
+interface SystemEntity extends AbstractEntity {
+	category: string
+	description: string
+	returns: string
+	default: string
+}
+
+interface Intent {
+	name?: string
+	entities?: EntityMap
+	examples: string[]
+	priority?: number
+	noEntityReset?: boolean
+}
+
+type EntityMap = {
+	[key: string]: AbstractEntity | Entity | SystemEntity
+};
+
+interface UserTurn {
+	id?: string
+	intent: string[] | Intent
+	bot:
+	| BotTurn
+	| DynamicBotTurn
+	| OrderTurn
+	| BridgeTurn
+	| string
+	| Array<BotTurn | DynamicBotTurn | OrderTurn | BridgeTurn | string>
+}
+
+interface Order {
+	type: string
+	confirmationText: string
+	merchantName: string
+	name: string
+	description: string
+}
+
+type ConditionMap = {
+	[key: string]: boolean | string | string[] | number | ConditionMap | undefined
+	NOT?: ConditionMap
+	OR?: ConditionMap
+	AND?: ConditionMap
+};
+
+type VariableMap = {
+	[key: string]: string | boolean | number | object | Array<string | boolean | number | object>
+};
+
+interface RichSay {
+	text: string | string[]
+	ssml?: string | string[]
+	cond?: ConditionMap
+	suggestions?: string[]
+	content?: Content | any
+	//| BasicCard | Image | BrowseCarousel | MediaObject | Table | List | Carousel
+}
+
+interface AbstractBotTurn {
+	id?: string
+	say?: string | RichSay | Array<string | RichSay>
+	repair?: boolean | "PARENT"
+	label?: string
+	goto?: string
+	cond?: ConditionMap
+	set?: VariableMap
+	expectShortAnswer?: boolean
+}
+
+interface BotTurn extends AbstractBotTurn {
+	say: string | RichSay | Array<string | RichSay>
+	user?: UserTurn[]
+}
+
+interface BridgeTurn extends AbstractBotTurn {
+	url?: string
+	params?: string[]
+	bot:
+	| BotTurn
+	| DynamicBotTurn
+	| OrderTurn
+	| BridgeTurn
+	| string
+	| Array<BotTurn | DynamicBotTurn | OrderTurn | BridgeTurn | string>
+	asyncWebhook?: boolean
+}
+
+interface DynamicBotTurn extends AbstractBotTurn {
+	url: string
+	user?: UserTurn[]
+	params?: string[]
+	asyncWebhook?: boolean
+}
+
+interface WebhookResponse {
+	say?: string
+	set?: VariableMap
+}
+
+interface OrderTurn extends AbstractBotTurn {
+	orderType: "BOOK" | "RESERVE" | "BUY" | "PLACE_ORDER" | "PAY" | "SEND" | "RESERVE" | "SCHEDULE" | "SUBSCRIBE"
+	name: string
+	description: string
+	imageUrl?: string
+	merchantName?: string
+	confirmationText: string
+	onConfirmed: BotTurn | DynamicBotTurn
+	onCancelled: BotTurn | DynamicBotTurn
+}
+
+function isOrderBotTurn(abstractTurn: AbstractBotTurn | BotTurn | DynamicBotTurn | OrderTurn | BridgeTurn) {
+	return abstractTurn && (abstractTurn as OrderTurn).onConfirmed !== undefined;
+}
+
+function isDynamicBotTurn(abstractTurn: AbstractBotTurn | BotTurn | DynamicBotTurn | OrderTurn | BridgeTurn) {
+	return (
+		abstractTurn && (abstractTurn as DynamicBotTurn).url !== undefined && (abstractTurn as BridgeTurn).bot === undefined
+	);
+}
+
+function isBridgeTurn(abstractTurn: AbstractBotTurn | BotTurn | DynamicBotTurn | OrderTurn | BridgeTurn) {
+	return abstractTurn && (abstractTurn as BridgeTurn).bot !== undefined;
+}
+
+function isDynamicEntity(abstractEntity: AbstractEntity | Entity | DynamicEntity) {
+	return abstractEntity && (abstractEntity as DynamicEntity).url !== undefined;
+}
+
+function isSystemEntity(abstractEntity: AbstractEntity | Entity | SystemEntity) {
+	return abstractEntity && (abstractEntity as Entity).enums === undefined;
+}
+
+function turnHasWebhook(abstractTurn: AbstractBotTurn | BotTurn | DynamicBotTurn | OrderTurn | BridgeTurn) {
+	return abstractTurn && (abstractTurn as DynamicBotTurn).url !== undefined;
+}
+
+interface LogTurn {
+	id: string
+	agentName: string
+	userInput: string
+	intentName: string
+	parameters: { [key: string]: any }
+	isFallback: boolean
+	isEndOfConversation: boolean
+	confidence: number
+	botReplies: string[]
+	timestamp: number
+}
+
+interface LogMessage {
+	sessionId: string
+	platform: string
+	turn: LogTurn
+	lastTurn?: LogTurn
+	text: string
+}
+
+// Language.ts
+
+enum Language {
+	ChineseCantonese = "zh-HK",
+	ChineseSimplified = "zh-CN",
+	ChineseTraditional = "zh-TW",
+	Danish = "da",
+	Dutch = "nl",
+	English = "en",
+	EnglishAustralia = "en-AU",
+	EnglishCanada = "en-CA",
+	EnglishGreatBritain = "en-GB",
+	EnglishIndia = "en-IN",
+	EnglishUS = "en-US",
+	French = "fr",
+	FrenchCanada = "fr-CA",
+	FrenchFrance = "fr-FR",
+	German = "de",
+	Hindi = "hi",
+	Indonesian = "id",
+	Italian = "it",
+	Japanese = "ja",
+	Korean = "ko",
+	Norwegian = "no",
+	Polish = "pl",
+	PortugueseBrazil = "pt-BR",
+	PortuguesePortugal = "pt",
+	Russian = "ru",
+	Spanish = "es",
+	SpanishLatinAmerica = "es-419",
+	SpanishSpain = "es-ES",
+	Swedish = "sv",
+	Thai = "th",
+	Turkish = "tr",
+	Ukranian = "uk",
+}
+
+// RichContent.ts
+
+ class Content {
+	type: string = "unknown";
+}
+
+// TODO: fix image any
+ class Card extends Content {
+	title: string;
+	description?: string;
+	subtitle?: string;
+	image?: any;
+	buttons?: Button[];
+	type: string = "card";
+
+	constructor({
+		title,
+		subtitle,
+		description,
+		image,
+		buttons
+	}: {
+		title: string
+		subtitle?: string
+		description?: string
+		image?: any
+		buttons?: Button[]
+	}) {
+		super();
+		this.title = title;
+		this.subtitle = subtitle;
+		this.description = description;
+		this.image = image;
+		this.buttons = buttons;
+	}
+}
+
+ class Button extends Content {
+	text: string;
+	url: string;
+	type: string = "button";
+
+	constructor(text: string, url: string) {
+		super();
+		this.text = text;
+		this.url = url;
+	}
+}
+
+ class List extends Content {
+	items: Item[];
+	title?: string;
+	image?: any;
+	type: string = "list";
+
+	constructor({ items, title, image }: { items: Item[]; title?: string; image?: any }) {
+		super();
+		this.items = items;
+		this.title = title;
+		this.image = image;
+	}
+}
+
+ class CarouselSelect extends Content {
+	items: Item[];
+	type: string = "carousel";
+
+	constructor(items: Item[]) {
+		super();
+		this.items = items;
+	}
+}
+
+ class Item extends Content {
+	title: string;
+	url?: string;
+	description?: string;
+	image?: any;
+	type: string = "item";
+
+	constructor({
+		url,
+		title,
+		description,
+		image
+	}: {
+		title: string
+		url?: string
+		description?: string
+		image?: any
+	}) {
+		super();
+		this.url = url;
+		this.title = title;
+		this.description = description;
+		this.image = image;
+	}
+}
+
+/*
+"info": {
+	object (SelectItemInfo)
+  },
+  "title": string,
+  "description": string,
+  "image": {
+	object (Image)
+  }
+  */
--- src/interfaces/richContent.ts
+++ src/interfaces/richContent.ts
@@ -1,14 +1,14 @@
 export class Content {
-  type: string = "unknown"
+  type: string = "unknown";
 }
 
 export class Card extends Content {
-  title: string
-  description?: string
-  subtitle?: string
-  image?: Image
-  buttons?: Button[]
-  type: string = "card"
+  title: string;
+  description?: string;
+  subtitle?: string;
+  image?: Image;
+  buttons?: Button[];
+  type: string = "card";
 
   constructor({
     title,
@@ -23,69 +23,69 @@ export class Card extends Content {
     image?: Image
     buttons?: Button[]
   }) {
-    super()
-    this.title = title
-    this.subtitle = subtitle
-    this.description = description
-    this.image = image
-    this.buttons = buttons
+    super();
+    this.title = title;
+    this.subtitle = subtitle;
+    this.description = description;
+    this.image = image;
+    this.buttons = buttons;
   }
 }
 
 export class Image extends Content {
-  url: string
-  alt: string
-  type: string = "image"
+  url: string;
+  alt: string;
+  type: string = "image";
 
   constructor(url: string, alt: string) {
-    super()
-    this.url = url
-    this.alt = alt
+    super();
+    this.url = url;
+    this.alt = alt;
   }
 }
 
 export class Button extends Content {
-  text: string
-  url: string
-  type: string = "button"
+  text: string;
+  url: string;
+  type: string = "button";
 
   constructor(text: string, url: string) {
-    super()
-    this.text = text
-    this.url = url
+    super();
+    this.text = text;
+    this.url = url;
   }
 }
 
 export class List extends Content {
-  items: Item[]
-  title?: string
-  image?: Image
-  type: string = "list"
+  items: Item[];
+  title?: string;
+  image?: Image;
+  type: string = "list";
 
   constructor({ items, title, image }: { items: Item[]; title?: string; image?: Image }) {
-    super()
-    this.items = items
-    this.title = title
-    this.image = image
+    super();
+    this.items = items;
+    this.title = title;
+    this.image = image;
   }
 }
 
 export class CarouselSelect extends Content {
-  items: Item[]
-  type: string = "carousel"
+  items: Item[];
+  type: string = "carousel";
 
   constructor(items: Item[]) {
-    super()
-    this.items = items
+    super();
+    this.items = items;
   }
 }
 
 export class Item extends Content {
-  title: string
-  url?: string
-  description?: string
-  image?: Image
-  type: string = "item"
+  title: string;
+  url?: string;
+  description?: string;
+  image?: Image;
+  type: string = "item";
 
   constructor({
     url,
@@ -98,11 +98,11 @@ export class Item extends Content {
     description?: string
     image?: Image
   }) {
-    super()
-    this.url = url
-    this.title = title
-    this.description = description
-    this.image = image
+    super();
+    this.url = url;
+    this.title = title;
+    this.description = description;
+    this.image = image;
   }
 }
 
